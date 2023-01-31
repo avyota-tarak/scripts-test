9cc48363ee2fc0c90f009b4d5674be402408176b
Tarak <tarak@avyota.com>
2023-01-31 18:28:11 +0100

    Initial commit message

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: "Yes",
+  env : "Prod",
+  NG_APP_SERVER_URL : process.env.NG_APP_SERVER_URL,
+  };
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: "NO",
+  env : "Staging",
+  NG_APP_SERVER_URL : process.env.NG_APP_SERVER_URL
+};
