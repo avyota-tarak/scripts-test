bbd3ae6bb9a4319ebe84f912a4f2be1f1a535789
Tarak <tarak@avyota.com>
2023-02-06 18:51:20 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: "Yes",
   env : "Prod",
   NG_APP_SERVER_URL : process.env.NG_APP_SERVER_URL,
-  Sec : "this is sec"
+  Sec : "this is sec 1 done in development"
   };
